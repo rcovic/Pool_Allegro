3156deaaae28fc106b3b52fb73803d93e9dc4188
Zaxorn <58526402+Zaxorn@users.noreply.github.com>
2019-12-12 16:31:34 +0100

    Update init.c

--- src/init.c
+++ src/init.c
@@ -6,9 +6,9 @@
 //-----------------------------------------------------------------------------
 // BITMAP OBJECTS DEFINITION (already commented in INIT.H)
 //-----------------------------------------------------------------------------
-BITMAP * parquet_bmp;
-BITMAP * pool_table_bmp;
-BITMAP * ball_panel_bmp;
+BITMAP*  parquet_bmp;
+BITMAP*  pool_table_bmp;
+BITMAP*  ball_panel_bmp;
 BITMAP* buffer_bmp;
 //-----------------------------------------------------------------------------
 // INIT_ENVIRONMENT FUNCTION: initialize allegro environment and ptask library
@@ -16,20 +16,20 @@ BITMAP* buffer_bmp;
 void init_environment(void){
     allegro_init();
     set_color_depth(desktop_color_depth());
-    set_gfx_mode(GFX_AUTODETECT_WINDOWED,RES_X,RES_Y,0,0);
+    set_gfx_mode(GFX_AUTODETECT_WINDOWED, RES_X, RES_Y, 0, 0);
     install_mouse();
     show_mouse(screen);
     install_keyboard();
-    ptask_init(SCHED_OTHER,GLOBAL,NO_PROTOCOL);
+    ptask_init(SCHED_OTHER, GLOBAL, NO_PROTOCOL);
 }
 //-----------------------------------------------------------------------------
 // INIT_BITMAPS FUNCTION: load bitmaps from img folder to bitmap objects
 //-----------------------------------------------------------------------------
 void init_bitmaps(void){
-    parquet_bmp = load_bitmap("img/parquet.bmp",NULL);
+    parquet_bmp = load_bitmap("img/parquet.bmp", NULL);
     pool_table_bmp = load_bitmap("img/poolTable.bmp", NULL);
-    ball_panel_bmp = load_bitmap("img/statsPanel.bmp",NULL);
-    buffer_bmp = create_bitmap(RES_X,RES_Y);
+    ball_panel_bmp = load_bitmap("img/statsPanel.bmp", NULL);
+    buffer_bmp = create_bitmap(RES_X, RES_Y);
 }
 //-----------------------------------------------------------------------------
 // INIT_GAME FUNCTION: calls all the INIT_FUNCTIONS when game starts
