6486e168bb66cde87b5bafd040df92e16b15d9c4
rcovic <rcovic7@outlook.it>
2019-12-12 17:27:52 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,23 @@
+//-----------------------------------------------------------------------------
+// MAIN.C - Initialize game, start tasks and ends game on user input
+//-----------------------------------------------------------------------------
+#include "init.h"
+//-----------------------------------------------------------------------------
+// MAIN function
+//-----------------------------------------------------------------------------
+int main(void){
+	//initialize game
+	init_game();
+
+	//create render task							
+	ptask_create_edf(render_task,P,RT,DL,NOW);
+
+	//wait for user input to end
+	while (!key[KEY_ESC]){};
+						
+	//close allegro environment
+	allegro_exit();
+
+	return 0;
+}
 
