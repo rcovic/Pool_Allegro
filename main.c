dd8630733e0b2b51a5a8ee57d5512dd1bace1b91
rcovic <rcovic7@outlook.it>
2019-12-13 02:08:07 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,11 +6,20 @@
 // MAIN function
 //-----------------------------------------------------------------------------
 int main(void){
+	int	i = 0;				//ball index
+
 	//initialize game
 	init_game();
 
 	//create render task							
 	ptask_create_edf(render_task,P,RT,DL,NOW);
+	
+	//create 16 ball tasks
+	for(i=0; i<1; ++i)
+		ptask_create_edf(ball_task,P,RT,DL,NOW);
+
+	//create user task
+	ptask_create_edf(user_task,P,RT,DL,NOW);
 
 	//wait for user input to end
 	while (!key[KEY_ESC]){};
@@ -20,4 +29,3 @@ int main(void){
 
 	return 0;
 }
-
