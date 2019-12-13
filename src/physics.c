265df3d54c533acc012283c07e3f6704d2f7dc16
rcovic <rcovic7@outlook.it>
2019-12-13 21:18:50 +0100

    Update physics.c

--- src/physics.c
+++ src/physics.c
@@ -1,1 +1,43 @@
+//-----------------------------------------------------------------------------
+// PHYSICS.C Implementation of physics methods used to calculate physics
+// parameters, like distances, angles ecc.
+//-----------------------------------------------------------------------------
+#include "init.h"
+#include "physics.h"
+//-----------------------------------------------------------------------------
+// GET_DISTANCE FUNCTION: get distance between 2 points saving x,y 
+// coordinates of projection of a point
+//-----------------------------------------------------------------------------
+double  get_distance(point* p3, int x1, int y1, int x2, int y2) {
+    double    distance;                             //stores return value
 
+    p3->x = x1 - x2;                                //get x projection
+    p3->y = y1 - y2;                                //get y projection
+    distance = sqrt(pow(p3->x, 2) + pow(p3->y, 2));    //calculate distance
+    return distance;
+}
+//-----------------------------------------------------------------------------
+// GET_ANGLE FUNCTION: get angle between 2 points calculated along x axis
+//-----------------------------------------------------------------------------
+double  get_angle(point p3, double distance) {
+    double  angle;                                  //stores return value
+
+    angle = acos(p3.x / distance) * 180 / PI;       //calculate angle
+
+    if(p3.y < 0)                                    //adjust angle
+        angle = (360 - abs(angle)) % 360;
+    return  angle;
+}
+//-----------------------------------------------------------------------------
+// GET_VECTOR_ANGLE FUNCTION: uses a vector value instead of 2 points
+//-----------------------------------------------------------------------------
+double  get_vector_angle(vector v, double module) {
+    double  angle;                                  //stores return value
+
+    //calculate angle using a 0.001 padding to avoid division on 0
+    angle = acos(((v.x) + 0.001) / (module + 0.001)) * 180 / PI;
+
+    if(v.y < 0)                                     //adjust angle
+        angle = (360 - abs(angle)) % 360;
+    return  angle;
+}
