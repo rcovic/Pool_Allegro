d502fd50cd1574f094577d004e528175e6dd4c32
Zaxorn <58526402+Zaxorn@users.noreply.github.com>
2019-12-14 02:46:27 +0100

    Update render.c

--- src/render.c
+++ src/render.c
@@ -8,11 +8,19 @@
 // which is then drawn on screen
 //-----------------------------------------------------------------------------
 void draw_on_buffer(){
+    int i = 0;      //ball index
+    
     draw_sprite(buffer_bmp,parquet_bmp,0,0);
     draw_sprite(buffer_bmp,pool_table_bmp,TABLE_X,TABLE_Y);
     draw_sprite(buffer_bmp,ball_panel_bmp, PANEL1_X, PANEL1_Y);
     //draw the bottom ball panel rotating the bitmap at 180 degrees
     rotate_sprite(buffer_bmp,ball_panel_bmp,PANEL2_X, PANEL2_Y, itofix(128));
+    
+    for (i=0; i<1; ++i)                     //draw the 16 balls
+        draw_sprite(buffer_bmp, ball_bmp[i], ball[i].p.x, ball[i].p.y);
+
+    if(user.state < WAKE_BALL)              //draw cue if user has to shot
+          pivot_sprite(buffer_bmp, cue_bmp, user.p.x, user.p.y, 0, user.wd, itofix(user.cue_angle));
 }
 //-----------------------------------------------------------------------------
 // RENDER_TASK FUNCTION- implementation of render task who periodically draws 
@@ -22,10 +30,10 @@ void render_task(void){
     //Code executed periodically
     while(1){
         draw_on_buffer();
-        scare_mouse();
-        draw_sprite(screen,buffer_bmp,0,0);
+        scare_mouse();                      //avoid mouse interfere with render
+        draw_sprite(screen,buffer_bmp,0,0); //bitmaps are rendered on screen
         unscare_mouse();
-        ptask_wait_for_period();
+        ptask_wait_for_period();            //synchronize task with period
     }
 }
 
