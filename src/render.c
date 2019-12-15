7f55255903d2f49044e0415dd999c9150fadee7a
Zaxorn <58526402+Zaxorn@users.noreply.github.com>
2019-12-15 21:12:13 +0100

    Update render.c

--- src/render.c
+++ src/render.c
@@ -7,20 +7,21 @@
 // DRAW_ON_BUFFER FUNCTION: draws all the bitmaps first on a buffer bitmap
 // which is then drawn on screen
 //-----------------------------------------------------------------------------
-void draw_on_buffer(){
-    int i = 0;      //ball index
-    
-    draw_sprite(buffer_bmp,parquet_bmp,0,0);
-    draw_sprite(buffer_bmp,pool_table_bmp,TABLE_X,TABLE_Y);
-    draw_sprite(buffer_bmp,ball_panel_bmp, PANEL1_X, PANEL1_Y);
+void    draw_on_buffer() {
+    int i = 0;                                      //ball index
+
+    draw_sprite(buffer_bmp, parquet_bmp, 0, 0);
+    draw_sprite(buffer_bmp, pool_table_bmp, TABLE_X, TABLE_Y);
+    draw_sprite(buffer_bmp, ball_panel_bmp, PANEL1_X, PANEL1_Y);
     //draw the bottom ball panel rotating the bitmap at 180 degrees
-    rotate_sprite(buffer_bmp,ball_panel_bmp,PANEL2_X, PANEL2_Y, itofix(128));
-    
-    for (i=0; i<1; ++i)                     //draw the 16 balls
+    rotate_sprite(buffer_bmp, ball_panel_bmp, PANEL2_X, PANEL2_Y, itofix(128));
+     
+    for (i=0; i<N_BALLS; ++i)                       //draw the 16 balls
         draw_sprite(buffer_bmp, ball_bmp[i], ball[i].p.x, ball[i].p.y);
 
-    if(user.state < WAKE_BALL)              //draw cue if user has to shot
+    if (user.state < WAKE_BALL)                     //draw cue if user has to shot
           pivot_sprite(buffer_bmp, cue_bmp, user.p.x, user.p.y, 0, user.wd, itofix(user.cue_angle));
+
 }
 //-----------------------------------------------------------------------------
 // RENDER_TASK FUNCTION- implementation of render task who periodically draws 
