9f810b17fff7cbec6163609862bbb4217b55d2cb
Zaxorn <58526402+Zaxorn@users.noreply.github.com>
2019-12-15 21:07:24 +0100

    Update physics.h

--- headers/physics.h
+++ headers/physics.h
@@ -16,12 +16,19 @@
 //-----------------------------------------------------------------------------
 // PHYSICS FUNCTION DECLARATIONS
 //-----------------------------------------------------------------------------
+//set angle between 0 and 360 degrees
+extern  void    adjust_angle(double* angle);
 //get distance between 2 points saving x,y coord. of projection along x axis
-extern  double  get_distance(point* p3, int x1, int y1, int x2, int y2);
+extern  double  get_dist(point* p3, int x1, int y1, int x2, int y2);
 //get angle between 2 points calculated along x axis
 extern  double  get_angle(point p3, double distance);
 //same as get_angle but uses a vector value instead of 2 points
 extern  double  get_vector_angle(vector v, double module);
+//calculate how much speed a ball gives to another
+extern  double  get_speed_given(int tid, double angle);
+//calculate bounce angle of a ball when collides with another ball
+extern  double  get_bounce_angle(double a1, double a2);
 
 #endif
+
 
