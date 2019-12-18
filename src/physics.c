d8e18eb1afbaa29f42a7a155ff4c1159a2cc41fc
Zaxorn <58526402+Zaxorn@users.noreply.github.com>
2019-12-18 17:29:00 +0100

    Update physics.c

--- src/physics.c
+++ src/physics.c
@@ -51,3 +51,77 @@ double  get_vector_angle(vector v, double module) {
         angle = (360 - abs(angle)) % 360;
     return  angle;
 }
+//-----------------------------------------------------------------------------
+// GET_SPEED_GIVEN FUNCTION: calculates how much speed a ball gives to another
+// inversely proportional to the difference between collision and move angle
+//-----------------------------------------------------------------------------
+double  get_speed_given(int i, double angle){
+    double  diff;                                   //diff between angles
+    double  speed_given;                            //return value
+
+    //calculate difference on a 0-360 degrees circumference
+    if (ball[i].angle > 270 && angle <90)
+        diff = abs(((int)(ball[i].angle - (angle + 360)) % 90));
+    else if (ball[i].angle < 90 && angle > 270)
+        diff = abs(((int)((ball[i].angle + 360) - angle) % 90));
+    else
+        diff = abs(((int)(ball[i].angle - (angle)) % 90));
+    
+    //Calculate speed given holding back a 10% value to avoid too static game
+    speed_given = (ball[i].speed * (1 - (diff / 90.0f)));
+    return speed_given * 0.9f;
+}
+//-----------------------------------------------------------------------------
+// GET_BOUNCE_ANGLE FUNCTION: calculate bounce angle of a ball when collides
+// with another ball based on ball direction angle and collision angle
+// generally calculating a 90 degrees back or forth as bounce
+//-----------------------------------------------------------------------------
+double  get_bounce_angle(double a1, double a2){
+    double bounce_angle;                            //return value
+
+    if ((a1 < a2 && !(a1 <= 90 && a2 >= 270)) || (a1 >= 270 && a2 <= 90))
+        bounce_angle = a2 - 90;
+    else if (a1 > a2 || (a1 <= 90 && a2 >= 270))
+        bounce_angle = a2 + 90;
+	//ball is pushed behind instead of perp. if hits a ball with
+	//collision angle equal to direction angle
+    else if (a1 == a2)								                        
+        bounce_angle = a2 + 180;
+    return bounce_angle;
+}
+//-----------------------------------------------------------------------------
+// DEGREE_TO_M FUNCTION: get the slope value given an angle
+//-----------------------------------------------------------------------------
+double degree_to_m(double angle){
+	double rad=angle*PI/180;
+	double m=tan(rad);
+	return m;
+}
+//-----------------------------------------------------------------------------
+// SET_TOP - set x end point given y TOP coordinate for a line with given m
+//-----------------------------------------------------------------------------
+void set_top(point *b, int x, int y, double m){
+		b->y=TOP_Y+5;
+		b->x = (((b->y - y)/m) + x) ;
+}
+//-----------------------------------------------------------------------------
+// SET_BOT - set x end point given y BOT coordinate for a line with given m
+//-----------------------------------------------------------------------------
+void set_bot(point *b, int x, int y, double m){   
+		b->y=BOT_Y-5;
+		b->x = (((b->y - y)/m) + x) ;
+}
+//-----------------------------------------------------------------------------
+// SET_LEFT - set y end point given x LEFT coordinate for a line with given m
+//-----------------------------------------------------------------------------
+void set_left(point *b, int x, int y, double m){   
+		b->x=LEFT_X+2;
+		b->y = (((b->x - x)*m) + y) ;
+}
+//-----------------------------------------------------------------------------
+// SET_RIGHT - set y end point given x RIGHT coordinate for a line with given m
+//-----------------------------------------------------------------------------
+void set_right(point *b, int x, int y, double m){   
+		b->x=RIGHT_X-2;
+		b->y = (((b->x - x)*m) + y) ;
+}
