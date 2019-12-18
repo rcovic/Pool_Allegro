0f38dab4027fc2ea0c44f63b4e670bc7d74ef238
Zaxorn <58526402+Zaxorn@users.noreply.github.com>
2019-12-18 17:17:05 +0100

    Update ball.c

--- src/ball.c
+++ src/ball.c
@@ -1,12 +1,14 @@
 //-----------------------------------------------------------------------------
 // BALL.C - contains definition of BALL_TASK and all its controlling functions
+// as collision with other balls, with borders or holes, and through the
+// PHYSICS library modifies periodically its params and updates its position
 //-----------------------------------------------------------------------------
 #include "init.h"
 #include "physics.h"
 
 //-----------------------------------------------------------------------------
-//RESET_COLLISION_PARAMETERS FUNCTION - reset parameters before checking
-// for collisions with other balls
+//RESET_COLLISION_PARAMETERS FUNCTION - reset parameters each period 
+//before checking collisions with other balls
 //-----------------------------------------------------------------------------
 void    reset_collision_parameters(int i) {
   ball[i].ld.x = 0;
@@ -22,14 +24,14 @@ void    reset_collision_parameters(int i) {
 // how many balls a ball collides with
 //-----------------------------------------------------------------------------
 int     check_ball_collision(int i) {
-    point  p3;                                  //projection point
-    double dist;                                //distance between two balls
-    double c_angle, b_angle, r_angle;           //collision,bounce,rev angle
-    int collision_counter = 0;                  //stores how many collision
-    int j;                                      //ball index
+    point   p3;                                 //projection point
+    double  dist;                               //distance between two balls
+    double  c_angle, b_angle, r_angle;          //used to manage collisions
+    int     collision_counter = 0;              //stores number of collisions
+    int     j;                                  //ball index
 
     for (j=0; j<N_BALLS; j++) {
-        if (ball[j].alive && j != i) {
+        if (ball[j].alive && j != i) {          //check only for ball on table
             dist = get_dist(&p3, ball[j].c.x, ball[j].c.y, ball[i].c.x, ball[i].c.y);
             //collision if distance between 2 ball centers is 2*ball_radius
             if (ball[i].pd[j] > dist && ball[i].cf[j] < 0 && dist < 30.0f) {
@@ -50,9 +52,9 @@ int     check_ball_collision(int i) {
                 ball[i].sg = get_speed_given(j, r_angle);
                 ball[i].gd.x += cos(r_angle * PI / 180) * ball[i].sg;
                 ball[i].gd.y += sin(r_angle * PI / 180) * ball[i].sg;
-                //set still flag if ball was still, so it can update coordinate
-                if (ball[i].still)
-                    ball[i].still = false;
+                //set flag if hit ball was still, so it can update coordinates
+                if (ball[j].still)
+                    ball[j].still = false;
             }
             ball[i].pd[j] = dist;               //update prev distance
         }
